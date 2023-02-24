1951e5151b782cce8f16be3f56229d51a523c27b
Artur Smith <arthur9smith7@gmail.com>
2023-02-24 12:03:10 +0100

    Written logs in all lifecycle methods. Added new activity

--- app/src/main/java/com/example/practicing_logs/MainActivity.kt
+++ app/src/main/java/com/example/practicing_logs/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.practicing_logs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainAct", "onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainAct", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainAct", "onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainAct", "onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainAct", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainAct", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainAct", "onDestroy")
+    }
+
+
 }
